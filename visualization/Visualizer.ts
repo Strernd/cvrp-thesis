417e1d306a81f4b8d0007048e08bdc07a04206b2
Bernd Strehl <bernd.strehl@the-white-label.com>
2018-05-01 18:12:54 +0200

    basic vis

---
+++ visualization/Visualizer.ts
@@ -0,0 +1,9 @@
+import { Solution } from "../types/Solution";
+import { Instance } from "../types/Instance";
+
+export namespace Visualizer {
+
+    export function render(instance: Instance, solution: Solution){
+        
+    }
+}
